671fd0270b2225b78f8a0ae0395a17758cd1a3ae
Duman <201605@astanait.edu.kz>
2025-03-19 19:44:14 +0500

    Add application configuration and event entity

--- src/main/java/kz/tickosh/event/EventServiceApplication.java
+++ src/main/java/kz/tickosh/event/EventServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventApplication {
+public class EventServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventApplication.class, args);
+		SpringApplication.run(EventServiceApplication.class, args);
 	}
 
 }
