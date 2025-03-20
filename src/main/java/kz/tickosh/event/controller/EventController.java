8909b9114f6ddfe9306b710dabee1b952a346368
Duman <201605@astanait.edu.kz>
2025-03-20 20:29:21 +0500

    Implement initial event management structure with controller, service, DTO, and repository

---
+++ src/main/java/kz/tickosh/event/controller/EventController.java
@@ -0,0 +1,12 @@
+package kz.tickosh.event.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/podpishi")
+public class EventController {
+
+}
---
+++ src/main/java/kz/tickosh/event/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package kz.tickosh.event.repository;
+
+import kz.tickosh.event.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
